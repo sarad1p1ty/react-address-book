660048a2e94fb9062fe628ad28e6e63a2c889102
Sarah <sarahwest84@gmail.com>
2018-10-22 08:57:02 +0100

    Updating to use material

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {Paper, Grid} from '@material-ui/core';
 import './App.css';
 import Form from './components/Form.js';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Form/>
-      </div>
-    );
-  }
+    render() {
+        return (<div className="App">
+            <Grid
+                lg="9"
+                md="9"
+                justify="center"
+                container="true">
+                <Paper>
+                    <Form/>
+                </Paper>
+            </Grid>
+        </div>);
+    }
 }
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,40 +1,68 @@
 import React from 'react';
+import {TextField, Button, FormControl}  from '@material-ui/core';
 
 const Form = () => (
     <form action="javascript:void(0)">
-        <label>
-            Name
-            <input type="text" name="contact_name" />
-        </label>
-        <label>
-            Address Line 1
-            <input type="text" name="contact_add1" />
-       </label>
-       <label>
-           Address Line 2
-           <input type="text" name="contact_add2" />
-       </label>
-       <label>
-
-           <input type="text" name="contact_town" />
-       </label>
-       <label>
-           Country
-           <input type="text" name="contact_country" />
-       </label>
-       <label>
-           Postcode
-           <input type="text" name="contact_postcode" />
-       </label>
-       <label>
-           Telephone
-           <input type="tel" name="contact_tel" />
-       </label>
-       <label>
-           Email
-           <input type="email" name="contact_email" />
-       </label>
-       <button type="submit" name="submit">Add</button>
+        <TextField
+            fullWidth="true"
+          id="contact_name"
+          label="Name"
+          type="text"
+          //value={this.state.name}
+          //onChange={this.handleChange('name')}
+        />
+        <TextField
+             fullWidth="true"
+            label="Address Line 1"
+            id="contact_add1"
+            type="text"
+        />
+       <TextField
+            fullWidth="true"
+           label="Address Line 2"
+           id="contact_add2"
+           type="text"
+       />
+       <TextField
+            fullWidth="true"
+            label="Town/City"
+            id="contact_town"
+            type="text"
+       />
+       <TextField
+            fullWidth="true"
+           label="Country"
+           id="contact_country"
+           type="text"
+       />
+       <TextField
+            fullWidth="true"
+           label="Postcode"
+           id="contact_postcode"
+           type="text"
+       />
+       <TextField
+            fullWidth="true"
+           label="Telephone"
+           id="contact_tel"
+           type="tel"
+       />
+       <TextField
+            fullWidth="true"
+           label="Email"
+           id="contact_email"
+           type="email"
+       />
+       <FormControl margin="normal">
+           <Button
+               type="submit"
+               id="contact_submit"
+               color="primary"
+               variant="contained"
+            >
+                   Submit
+           </Button>
+        </FormControl>
     </form>
 );
 
