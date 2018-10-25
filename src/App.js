5ecb0cd49b5bfe6c9a242ea7b2ada037217c8c06
Sarah Jane West <sarahjane@openobjects.com>
2018-10-25 18:59:27 +0100

    Styling change

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import {Paper, Grid} from '@material-ui/core';
 import './App.css';
 import Form from './components/Form.js';
 
+const styles = theme => ({
+    root: {
+    }
+});
+
 class App extends Component {
     render() {
-        return (<div className="App">
-            <Grid
-                lg="9"
-                md="9"
-                justify="center"
-                container="true">
+        return <div className="App">
+            <Grid container="true" justify="center">
+              <Grid lg="9" md="9" container="true">
                 <Paper>
-                    <Form/>
+                  <Form />
                 </Paper>
+              </Grid>
             </Grid>
-        </div>);
+          </div>;
     }
 }
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,69 +1,73 @@
-import React from 'react';
-import {TextField, Button, FormControl}  from '@material-ui/core';
+import React, {Component} from 'react';
+import {TextField, Button, FormControl} from '@material-ui/core';
 
-const Form = () => (
-    <form action="javascript:void(0)">
-        <TextField
-            fullWidth="true"
-          id="contact_name"
-          label="Name"
-          type="text"
-          //value={this.state.name}
-          //onChange={this.handleChange('name')}
-        />
-        <TextField
-             fullWidth="true"
-            label="Address Line 1"
-            id="contact_add1"
-            type="text"
-        />
-       <TextField
-            fullWidth="true"
-           label="Address Line 2"
-           id="contact_add2"
-           type="text"
-       />
-       <TextField
-            fullWidth="true"
-            label="Town/City"
-            id="contact_town"
-            type="text"
-       />
-       <TextField
-            fullWidth="true"
-           label="Country"
-           id="contact_country"
-           type="text"
-       />
-       <TextField
-            fullWidth="true"
-           label="Postcode"
-           id="contact_postcode"
-           type="text"
-       />
-       <TextField
-            fullWidth="true"
-           label="Telephone"
-           id="contact_tel"
-           type="tel"
-       />
-       <TextField
-            fullWidth="true"
-           label="Email"
-           id="contact_email"
-           type="email"
-       />
-       <FormControl margin="normal">
-           <Button
-               type="submit"
-               id="contact_submit"
-               color="primary"
-               variant="contained"
-            >
-                   Submit
-           </Button>
-        </FormControl>
-    </form>
-);
+class Form extends Component {
+    render() {
+        return (
+            <form autocomplete="off" onSubmit={e=>e.preventDefault}>
+                <TextField
+                    fullWidth="true"
+                    id="contact_name"
+                    label="Name"
+                    type="text"
+                    //value={this.state.name}
+                    //onChange={this.handleChange('name')}
+                />
+                <TextField
+                    fullWidth="true"
+                    label="Address Line 1"
+                    id="contact_add1"
+                    type="text"
+                />
+                <TextField
+                    fullWidth="true"
+                    label="Address Line 2"
+                    id="contact_add2"
+                    type="text"
+                />
+                <TextField
+                    fullWidth="true"
+                    label="Town/City"
+                    id="contact_town"
+                    type="text"
+                />
+                <TextField
+                    fullWidth="true"
+                    label="Country"
+                    id="contact_country"
+                    type="text"
+                />
+                <TextField
+                    fullWidth="true"
+                    label="Postcode"
+                    id="contact_postcode"
+                    type="text"
+                />
+                <TextField
+                    fullWidth="true"
+                    label="Telephone"
+                    id="contact_tel"
+                    type="tel"
+                />
+                <TextField
+                    fullWidth="true"
+                    label="Email"
+                    id="contact_email"
+                    type="email"
+                />
+                    <FormControl margin="normal">
+                        <Button
+                            type="submit"
+                            id="contact_submit"
+                            color="primary"
+                            variant="contained"
+                        >
+                        Submit
+                    </Button>
+                </FormControl>
+            </form>
+        )
+    }
+}
 
 export default Form;
